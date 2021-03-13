b921efa2c11189fcc25ccdd36d853f6d90b8ca03
Ashish <ashishDongol32@gmail.com>
2021-03-13 17:29:31 +0545

    contact get done and contact clear done

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -4,25 +4,45 @@ import {v4 as uuid} from 'uuid';
 import ContactContext from './contactContext';
 import ContactReducer from './contactReducer';
 import {
+    GET_CONTACTS,
     ADD_CONTACT,
     DELETE_CONTACT,
     SET_CURRENT,
     CLEAR_CURRENT,
     UPDATE_CONTACT,
     FILTER_CONTACTS,
+    CLEAR_CONTACTS,
     CLEAR_FILTER,
     CONTACT_ERROR
 } from '../types';
 
 const ContactState = props => {
     const initialState = {
-        contacts: [],
+        contacts: null,
         current: null,
         filtered: null,
         error: null
     }
 
     const [state, dispatch] = useReducer(ContactReducer, initialState);
+
+    // Get Contacts
+    const getContacts = async () => {
+
+        try {
+            const res = await axios.get('/api/contacts');
+
+            dispatch({ 
+                type: GET_CONTACTS, 
+                payload: res.data
+            });
+        } catch (err) {
+            dispatch({
+                type: CONTACT_ERROR,
+                payload: err.resposne.msg
+            })
+        }
+    }
 
     // Add Contact
     const addContact = async contact => {
@@ -38,7 +58,10 @@ const ContactState = props => {
         try {
             const res = await axios.post('/api/contacts', contact, config);
 
-            dispatch({ type: ADD_CONTACT, payload: res.data});
+            dispatch({ 
+                type: ADD_CONTACT, 
+                payload: res.data
+            });
         } catch (err) {
             dispatch({
                 type: CONTACT_ERROR,
@@ -56,6 +79,13 @@ const ContactState = props => {
     // Delete Contact
     const deleteContact = id => {
         dispatch({ type: DELETE_CONTACT, payload: id});
+    }
+
+    // Clear Contacts
+    const clearContacts = () => {
+        dispatch({
+            type: CLEAR_CONTACTS
+        });
     }
 
     // Set Current Contact
@@ -90,13 +120,15 @@ const ContactState = props => {
             current: state.current,
             filtered: state.filtered,
             error: state.error,
+            getContacts,
             addContact,
             deleteContact,
             setCurrent,
             clearCurrent,
             updateContact,
             filterContacts,
-            clearFilter
+            clearFilter,
+            clearContacts
         }}>
             { props.children }
         </ContactContext.Provider>
